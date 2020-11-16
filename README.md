1fbd2e2ce0cde49ac20f2dadcd21438c1e4d602f
Vladislav Kalmykov <davert-tanz@yandex.ru>
2020-11-16 11:36:53 +0500

    Добавлена возможность кастомизации скрипта при установке

--- README.md
+++ README.md
@@ -7,12 +7,28 @@
 Устанавливает хук _prepare-commit-msg_ в текущий репозиторий git'а.
 Если до этого уже существовал данный хук, то он переименовывается в резервный (например, _prepare-commit-msg.reserve_).
 
+## Опции
+
+* `--out` - настройка параметра `GIT_PREPARE_COMMIT_OUT_FORMAT`
+* `--template` - настройка параметра `GIT_PREPARE_COMMIT_REGEXP_BRANCH_TEMPLATE`
+* `--search` - настройка параметра `GIT_PREPARE_COMMIT_REGEXP_FOR_SEARCH`
+
 ## Установка и выполнение
 
+Пример по умолчанию:
 ```bash
 $ cd <repository_dir>
 $ wget -qO - https://raw.githubusercontent.com/Davert94/install-hook/master/install-hook.sh | bash
 
 // Или если wget не установлен в системе
 $ curl -s https://raw.githubusercontent.com/Davert94/install-hook/master/install-hook.sh | bash
+```
+
+Пример с передаваемыми параметрами:
+```bash
+$ cd <repository_dir>
+$ wget -qO - https://raw.githubusercontent.com/Davert94/install-hook/master/install-hook.sh | bash -s -- --out="{issue #__value__}" --template="feature/\d+-?"
+
+// Или если wget не установлен в системе
+$ curl -s https://raw.githubusercontent.com/Davert94/install-hook/master/install-hook.sh | bash -s -- --out="{issue #__value__}" --template="feature/\d+-?"
 ```
--- install-hook.sh
+++ install-hook.sh
@@ -7,13 +7,11 @@ RESERVE_FILE="${DESTINATION_FILE}.reserve"
 
 HTTP_CLIENT=$(which wget 2>/dev/null)
 IS_CURL=0
-
 if [[ -z ${HTTP_CLIENT} ]];
 then
     IS_CURL=1
 fi
 
-# Сохраним старый хук на всякий
 mv "${DESTINATION_FILE}" "${RESERVE_FILE}" 2>/dev/null
 
 if [[ ${IS_CURL} -eq 1 ]];
@@ -28,6 +26,36 @@ REQUEST_RESULT=$?
 if [[ ${REQUEST_RESULT} -eq 0 ]];
 then
     chmod +x "${DESTINATION_FILE}"
+
+    while true
+    do
+      if [[ -z "${1}" ]];
+      then
+        break
+      fi
+
+      if [[ ! -f "${HOME}/.config/prepare.config" ]];
+      then
+        mkdir -p "${HOME}/.config"
+      fi
+
+      case "${1}" in
+        --out=*)
+            echo "GIT_PREPARE_COMMIT_OUT_FORMAT=\"${1##*=}\"" >> "${HOME}/.config/prepare.config"
+          ;;
+        --template=*)
+            echo "GIT_PREPARE_COMMIT_REGEXP_BRANCH_TEMPLATE=\"${1##*=}\"" >> "${HOME}/.config/prepare.config"
+          ;;
+        --search=*)
+            echo "GIT_PREPARE_COMMIT_REGEXP_FOR_SEARCH=\"${1##*=}\"" >> "${HOME}/.config/prepare.config"
+          ;;
+        *)
+            echo 'Incorrect parameters' && exit 1
+          ;;
+      esac
+
+      shift
+    done
 
     echo -e "Hook \"${DESTINATION_FILE}\" successfully installed!"
 else
